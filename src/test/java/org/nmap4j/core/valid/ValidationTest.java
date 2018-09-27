06b58aa3345d2f977553685a026b93e61f0c491e
ronen <narkisr.com>
2018-09-27 21:40:33 +1000

    Adding hosts validation fixing https://github.com/narkisr/nmap4j/issues/9

--- src/test/java/org/nmap4j/core/valid/ValidationTest.java
+++ src/test/java/org/nmap4j/core/valid/ValidationTest.java
@@ -1,4 +1,25 @@
 package org.nmap4j.core.valid;
 
+import org.junit.Test;
+import org.nmap4j.valid.HostsInputValidator;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class ValidationTest {
+
+    @Test
+    public void validHosts() {
+        assertTrue(new HostsInputValidator().valid("8.8.4.3"));
+        assertTrue(new HostsInputValidator().valid("8.8.4.3/32"));
+        assertTrue(new HostsInputValidator().valid("google.com"));
+    }
+
+    @Test
+    public void nonValidHosts() {
+        boolean b;
+        assertFalse(new HostsInputValidator().valid("8.8.4"));
+        assertFalse(new HostsInputValidator().valid("8.8.4.3/322"));
+        assertFalse(new HostsInputValidator().valid("google.com.8.8.4.3"));
+    }
 }
